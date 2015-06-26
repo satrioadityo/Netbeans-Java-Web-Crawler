9a79fd3e9e1ba4c2ab9c26df3d088131196dd363
asdf <asdf@gmail.com>
2015-06-26 09:12:39 +0700

    tidying code indentation, it takes no effect on program

--- src/com/java/crawler/basdat/Spider.java
+++ src/com/java/crawler/basdat/Spider.java
@@ -19,47 +19,47 @@ public class Spider {
     private SpiderLeg leg;
 
     private void initSeeding(String seed) {
-            // TODO menginisialisasi URL pertama yang akan di-crawl
-            listPageToVisit.add(seed);
+        // TODO menginisialisasi URL pertama yang akan di-crawl
+        listPageToVisit.add(seed);
     }
 
     private void seeding() {
-            // TODO menambahkan semua link hasil crawl ke listPageToVisit
-            this.listPageToVisit.addAll(leg.getLinks());
+        // TODO menambahkan semua link hasil crawl ke listPageToVisit
+        this.listPageToVisit.addAll(leg.getLinks());
     }
 
     public void startCrawl() {
-            initSeeding(seed);
+        initSeeding(seed);
 
-            while(this.listPageVisited.size() < LIMIT){
-                    String currentUrl;
-                    leg = new SpiderLeg();
+        while(this.listPageVisited.size() < LIMIT){
+            String currentUrl;
+            leg = new SpiderLeg();
 
-                    currentUrl = this.getNextUrl();
-                    System.out.println("current url = " + currentUrl);
+            currentUrl = this.getNextUrl();
+            System.out.println("current url = " + currentUrl);
 
-                    leg.crawl(currentUrl); // proses crawling
-                    this.listPageVisited.add(currentUrl); // setelah crawling add ke listPageVisited
-                    seeding(); // tambah semua link hasil crawling ke listPageToVisit
+            leg.crawl(currentUrl); // proses crawling
+            this.listPageVisited.add(currentUrl); // setelah crawling add ke listPageVisited
+            seeding(); // tambah semua link hasil crawling ke listPageToVisit
 
-                    for(String s : this.listPageVisited){
-                            System.err.println(s + " yeah !");
-                    }
+            for(String s : this.listPageVisited){
+                    System.err.println(s + " yeah !");
             }
-            // proses crawling sudah selesai
-            System.out.println("\n**Done** Visited " + this.listPageVisited.size() + " web page(s)");
+        }
+        // proses crawling sudah selesai
+        System.out.println("\n**Done** Visited " + this.listPageVisited.size() + " web page(s)");
     }
 
     private String getNextUrl() {
-            String nextUrl;
-            do {
-                    nextUrl = this.listPageToVisit.remove(0);
-            } while(this.listPageVisited.contains(nextUrl)); // melakukan dequeue sampai link yg belum dikunjungi didapat
+        String nextUrl;
+        do {
+            nextUrl = this.listPageToVisit.remove(0);
+        } while(this.listPageVisited.contains(nextUrl)); // melakukan dequeue sampai link yg belum dikunjungi didapat
 
-            return nextUrl; // return link yang belum dikunjungi untuk dicrawl
+        return nextUrl; // return link yang belum dikunjungi untuk dicrawl
     }
 
     public void setSeed(String seed) {
-            this.seed = seed;
+        this.seed = seed;
     }
 }
